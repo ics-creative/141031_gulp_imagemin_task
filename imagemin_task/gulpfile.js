35954325c638393e274500037e0f18eb1583c0c4
ics-kano <takeshi.kano@ics-web.jp>
2016-01-13 23:18:23 +0900

    lab-dataにアップしていたタスクファイルを移行

---
+++ imagemin_task/gulpfile.js
@@ -0,0 +1,14 @@
+// gulpプラグインの読みこみ
+var gulp = require('gulp');
+// 画像を圧縮するプラグインの読み込み
+var imagemin = require("gulp-imagemin");
+
+/*
+ * imagesフォルダー以下のpng画像を圧縮して
+ * minified_imageフォルダに保存するタスク
+ */
+gulp.task("imageMinTask", function() { // 「imageMinTask」という名前のタスクを登録
+    gulp.src("images/*.png")    // imagesフォルダ以下のpng画像を取得
+        .pipe(imagemin())   // 画像の圧縮処理を実行
+        .pipe(gulp.dest("minified_image/"));   // minified_imageフォルダ以下に保存
+});
