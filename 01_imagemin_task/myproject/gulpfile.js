ba93cc7ce61996119511c8a3a0d40640b125133a
鹿野壮 <takeshi.kano@ics-web.jp>
2017-07-20 13:42:54 +0900

    コードフォーマット

--- 01_imagemin_task/myproject/gulpfile.js
+++ 01_imagemin_task/myproject/gulpfile.js
@@ -1,11 +1,11 @@
 // gulpプラグインの読みこみ
-var gulp = require('gulp');
+var gulp = require("gulp");
 // 画像を圧縮するプラグインの読み込み
 var imagemin = require("gulp-imagemin");
 
 // imagesフォルダー以下のpng画像を圧縮してminified_imageフォルダに保存するタスクの定義
-gulp.task("imageMinTask", function() {  // 「imageMinTask」という名前のタスクを登録
-    gulp.src("images/*.png")    // imagesフォルダ以下のpng画像を取得
-        .pipe(imagemin())   // 画像の圧縮処理を実行
-        .pipe(gulp.dest("minified_image/"));    // minified_imageフォルダ以下に保存
+gulp.task("imageMinTask", function () {  // 「imageMinTask」という名前のタスクを登録
+  gulp.src("images/*.png")    // imagesフォルダ以下のpng画像を取得
+    .pipe(imagemin())   // 画像の圧縮処理を実行
+    .pipe(gulp.dest("minified_image/"));    // minified_imageフォルダ以下に保存
 });
